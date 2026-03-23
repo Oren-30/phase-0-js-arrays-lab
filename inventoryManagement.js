d622a56cf4c1d734d8f73c205fa0c6c230c548f0
sirkiruthi <orencaroli@gmail.com>
2026-03-23 21:42:23 +0300

    Adding arrays to the inventoryManagement.js file

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,7 +1,24 @@
 // Write your code here
-
-
-
+// Initialize the products array
+let products = ["Laptop", "Smartphone", "Tablet"];
+//log the first product
+function logFirstProduct() {
+  console.log(products[0]);
+}
+//add a new product to the end of the array
+function addProduct(product) {
+  products.push(product);
+}
+//update the name of the second product
+function updateProductName(newName) {
+  if (products.length >= 2) {
+    products[1] = newName;
+  }
+}
+//remove the last product from the array
+function removeLastProduct() {
+  products.pop();
+}
 // Export the necessary parts for testing
 module.exports = {
   logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
